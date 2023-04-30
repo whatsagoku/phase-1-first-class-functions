39d1134e2b878bbdcedca4bcd88c2ca877632a93
whatsagoku <jefferyshutchinson@gmail.com>
2023-04-30 07:59:01 -0400

    Whats the mile high club?

---
+++ index.js
@@ -0,0 +1,14 @@
+function receivesAFunction(callback) {
+    callback();
+}
+
+function returnsANamedFunction() {
+    return function namedFunction() {
+            console.log('This is a named function')
+        }
+    }
+function returnsAnAnonymousFunction() {
+    return function () {
+        console.log('This is an anonymous function')
+    }
+}
